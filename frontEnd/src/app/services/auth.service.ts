b8250a26fe4a991fa7c9ca15d765c77bdf0aa85a
ossama-ferjani <ossama.ferjani@insat.ucar.tn>
2025-01-24 17:06:14 +0100

    add jwt-auth-Angular

--- frontEnd/src/app/services/auth.service.ts
+++ frontEnd/src/app/services/auth.service.ts
@@ -1,34 +1,62 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
-import { Observable, tap } from 'rxjs';
+import { Observable } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   public loggedIn: boolean = false;
   public isAdmin: boolean = false;
   public isProf: boolean = false;
-  public name: string = "";
-  public token: string = "";
+  public name: string = '';
+  public token: string = '';
   public id: number = 0;
 
-  constructor(private http: HttpClient,private cookieService: CookieService) { }
+  constructor(private http: HttpClient, private cookieService: CookieService) {}
 
   login(username: string, password: string): Observable<any> {
     const loginData = { username, password };
-    return this.http.post<any>(environment.backendHost +'/auth/login', loginData);
+    return this.http
+      .post<any>(environment.backendHost + '/auth/login', loginData)
+      .pipe(shareReplay(1)); // Cache the response for multiple subscriptions
   }
 
-  logout(id:number): Observable<boolean> {
-    // remove cookies
-    this.cookieService.delete('username');
-this.cookieService.delete('userId');
-// Remove more cookies if needed
+  saveAuthData(token: string, userData: any): void {
+    this.token = token;
+    this.loggedIn = true;
+    this.isAdmin = userData.admin;
+    this.isProf = userData.enseignant;
+    this.name = `${userData.nom} ${userData.prenom}`;
+    this.id = userData.id;
 
-    return this.http.get<boolean>(environment.backendHost +'/auth/logout/'+id)
-    }
+    // Store the token and user data in cookies or localStorage
+    this.cookieService.set('authToken', token);
+    this.cookieService.set('username', this.name);
+    this.cookieService.set('userId', String(this.id));
+    this.cookieService.set('role', userData.admin ? 'Administrateur' : 'Enseignant');
+  }
 
+  getAuthToken(): string {
+    return this.cookieService.get('authToken');
+  }
+
+  logout(): void {
+    // Remove cookies and reset the state
+    this.cookieService.deleteAll();
+    this.loggedIn = false;
+    this.token = '';
+    this.id = 0;
+    this.name = '';
+    this.isAdmin = false;
+    this.isProf = false;
+  }
+
+  isAuthenticated(): boolean {
+    // Check if the user is logged in based on the presence of a token
+    return !!this.cookieService.get('authToken');
+  }
 }
